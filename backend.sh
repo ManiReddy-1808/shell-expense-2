a05e3d24f759fb1b722439724855ff020c543e2d
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-02 11:40:30 +0530

    Added new code

--- backend.sh
+++ backend.sh
@@ -63,11 +63,10 @@ else
     VALIDATE $? "Installing mysql"
 fi
 
-mysql -h $MYSQL_HOST -uroot -p${mysql_root_password} -e 'use transactions' &>>$LOGS_FILE
-if [ $? -eq 0 ]; then
+if mysql -h "$MYSQL_HOST" -uroot -p"${mysql_root_password}" -e 'use transactions' &>>$LOGS_FILE; then
     echo -e "Backend schema already Loaded ... $Y SKIPPING $N"
 else
-    mysql -h $MYSQL_HOST -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGS_FILE
+    mysql -h "$MYSQL_HOST" -uroot -p"${mysql_root_password}" < /app/schema/backend.sql &>>$LOGS_FILE
     VALIDATE $? "Loading backend schema"
 fi
 
